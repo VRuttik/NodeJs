553aff203f811c65b5a3f95277dc8f29661b7ffb
Vruttik Moragha <97348689+VRuttik@users.noreply.github.com>
2023-09-23 14:16:55 +0530

    Create index2.js
    Asynchronous FS Core Modules || Reading and Writing File.

---
+++ index2.js
@@ -0,0 +1,19 @@
+
+const fs = require("fs");
+
+
+fs.writeFile('read.txt', "Today is a awesome day :)", (err) =>{
+    console.log("file is created");
+    console.log(err);
+});
+
+
+
+    fs.appendFile('read.txt', ' Plz like and share and subs my youtub channel.!!', (err) => {
+    console.log('task completed');
+    });
+    
+
+    fs.readFile('read.txt','UTF-8', (err, data) => {
+        console.log(data);
+    });
